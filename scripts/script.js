6d631c68e2af880ed0538f5e2873f7cc8a3670ae
Leeyeyun <165612381+Leeyeyun@users.noreply.github.com>
2024-07-15 18:07:57 +0900

    이예윤 포트폴리오 - 작업 40%

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+//nav hover 시 글씨 나타나기
+const nav = document.querySelector('nav')
+const nav_a = nav.querySelectorAll('nav > a')
+for(let i of nav_a){
+    i.addEventListener('mouseover',(e)=>{
+        e.preventDefault();
+        console.log(i.children[0])
+        i.children[0].style.marginRight = '0';
+        i.children[0].style.opacity = '1'
+    })
+    i.addEventListener('mouseout',(e)=>{
+        e.preventDefault();
+        i.children[0].style.marginRight = '-24px';
+        i.children[0].style.opacity = '0'
+    })
+}
+
+//contents hover 애니메이션
+const contents_wrap = document.querySelector('.contents_wrap')
+const contents_a = contents_wrap.querySelectorAll('.contents_wrap > a')
+console.log(contents_a[0].children)
+for(let i of contents_a){
+    i.addEventListener('mouseover',(e)=>{
+        e.preventDefault();
+        i.children[2].style.opacity = '0'; //hover아이콘 숨기기
+        i.children[3].style.opacity = '1'; //썸네일 img 나타나기
+        i.children[3].style.right = '-70px'; //썸네일 img 오른쪽으로 등장
+        i.style.backgroundColor = '#0c0c0c';
+        console.log(i.children[0], i.children[1])
+        i.children[0].style.color = '#fff'
+        i.children[1].style.color = '#fff'
+    })
+    i.addEventListener('mouseout',(e)=>{
+        e.preventDefault();
+        i.children[2].style.opacity = '1';
+        i.children[3].style.opacity = '0';
+        i.children[3].style.right = '0';
+        i.style.backgroundColor = '';
+        i.children[0].style.color = '#0c0c0c'
+        i.children[1].style.color = '#0c0c0c'
+    })
+}
