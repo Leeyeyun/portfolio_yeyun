4f72540f386cbc756900229737f6a9509061f6ea
Leeyeyun <165612381+Leeyeyun@users.noreply.github.com>
2024-07-16 14:52:12 +0900

    contents-motion hover 시 영상 등장 css 작업

--- scripts/script.js
+++ scripts/script.js
@@ -18,27 +18,47 @@ for(let i of nav_a){
 //contents hover 애니메이션
 const contents_wrap = document.querySelector('.contents_wrap')
 const contents_a = contents_wrap.querySelectorAll('.contents_wrap > a')
-console.log(contents_a[0].children)
+const bg_video = document.querySelector('video')
+const contents_a_m = contents_a[3]
+console.log(contents_a[0].children.length, contents_a[3].children.length)
 for(let i of contents_a){
-    i.addEventListener('mouseover',(e)=>{
-        e.preventDefault();
-        i.children[2].style.opacity = '0'; //hover아이콘 숨기기
-        i.children[3].style.opacity = '1'; //썸네일 img 나타나기
-        i.children[3].style.right = '-70px'; //썸네일 img 오른쪽으로 등장
-        i.style.backgroundColor = '#0c0c0c';
-        console.log(i.children[0], i.children[1])
-        i.children[0].style.color = '#fff'
-        i.children[1].style.color = '#fff'
-    })
-    i.addEventListener('mouseout',(e)=>{
-        e.preventDefault();
-        i.children[2].style.opacity = '1';
-        i.children[3].style.opacity = '0';
-        i.children[3].style.right = '0';
-        i.style.backgroundColor = '';
-        i.children[0].style.color = '#0c0c0c'
-        i.children[1].style.color = '#0c0c0c'
-    })
+    if (i.children.length == 4){
+        i.addEventListener('mouseover',(e)=>{
+            e.preventDefault();
+            i.children[2].style.opacity = '0'; //hover아이콘 숨기기
+            i.children[3].style.opacity = '1'; //썸네일 img 나타나기
+            i.children[3].style.right = '-70px'; //썸네일 img 오른쪽으로 등장
+            i.style.backgroundColor = '#0c0c0c';
+            i.children[0].style.color = '#fff'
+            i.children[1].style.color = '#fff'
+        })
+        i.addEventListener('mouseout',(e)=>{
+            e.preventDefault();
+            i.children[2].style.opacity = '1';
+            i.children[3].style.opacity = '0';
+            i.children[3].style.right = '0';
+            i.style.backgroundColor = '';
+            i.children[0].style.color = '#0c0c0c'
+            i.children[1].style.color = '#0c0c0c'
+        })
+    }else {
+        i.addEventListener('mouseover',()=>{
+            i.children[2].style.opacity = '0'; //hover아이콘 숨기기
+            i.children[0].style.color = '#fff'
+            i.children[1].style.color = '#fff'
+            bg_video.style.opacity = '1';
+            contents_a_m.children[0].style.color = '#fff';
+            contents_a_m.children[1].style.color = '#fff';
+        })
+        i.addEventListener('mouseout',()=>{
+            i.children[2].style.opacity = '1';
+            i.children[0].style.color = '#0c0c0c'
+            i.children[1].style.color = '#0c0c0c'
+            bg_video.style.opacity = '';
+            contents_a_m.children[0].style.color = '#0c0c0c';
+            contents_a_m.children[1].style.color = '#0c0c0c';
+        })
+    }
 }
 
 //swiper ux
